38103fd71b0f1004b47aec3aa6bb3c00451f7e54
Abel <butterball6943@gmail.com>
2022-02-28 12:56:01 -0700

    https://github.com/acarrra61/demo1.git

--- backend/views.py
+++ backend/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Creating a view for requesting hard drives
 def index(request):
-    return HttpResponse("May I request, for hard drives")
+    #return HttpResponse("Homepage")
+    #return render(request, 'index.html')
+    #template = loader.get_template('index.html')
+    #return HttpResponse(template.render({}, request))
+    return render(request, 'request.html')
